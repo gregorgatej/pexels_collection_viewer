6bcc67cfc5d95b68edc666850c152952c8d5e6f4
Gregor Gatej <gregor.gatej@gmail.com>
2026-03-23 17:27:49 +0100

    feat: fetch collection photos from the home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+    return if params[:collection_id].blank?
+
+    @collection_id = params[:collection_id].strip
+    @photos = PexelsClient.new.collection_photos(@collection_id)
+  rescue PexelsClient::Error => e
+    @error_message = e.message
+    @photos = []
   end
 end
